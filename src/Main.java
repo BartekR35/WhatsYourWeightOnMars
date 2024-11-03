83859ef64836ff51ae3e21c4b2aef3a82074af6d
BartekR35 <bartosz.rainka98@gmail.com>
2024-11-03 19:27:51 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Mars gravity is about 38% of Earth's. Write a program which will count Your weight on Mars
+// Plus variable type conversions
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+      float EarthWRainka = 100;
+      float MarsWRainka = (float) (EarthWRainka * 0.38);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+      System.out.println(MarsWRainka);
+
+      // some data type conversions below
+
+      double RandomVarRainka = 1.31415925;
+
+      //double -> BigDecimal and limit to 4 zeros after coma
+
+      BigDecimal RoundedRandomVarRainka = BigDecimal.valueOf(RandomVarRainka).setScale(4, RoundingMode.HALF_UP);
+
+      System.out.println(RoundedRandomVarRainka);
+
+      // double -> int
+
+      double SomeDigitRainka = 63.340;
+
+      SomeDigitRainka = (int) SomeDigitRainka;;
+
+      // int -> char
+
+      SomeDigitRainka = (char) SomeDigitRainka;
+
+      System.out.println(SomeDigitRainka);;
+
+      //Some math on variable
+
+      SomeDigitRainka = (double) (SomeDigitRainka);
+
+      SomeDigitRainka = SomeDigitRainka * 5.4;
+
+      System.out.println(SomeDigitRainka);
+
+
     }
 }
